949d423c5c0fcca391d89ac8e9e063eb4961a839
dsofadog <delaney@so.fa.dog>
2021-02-16 04:58:55 +0200

    check valid mobile numbers

--- test/models/mobile_test.rb
+++ test/models/mobile_test.rb
@@ -4,4 +4,22 @@ class MobileTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "will create +4407123456789" do
+   mobile= Mobile.create! number: "+447123456789"
+   assert_not_nil(mobile)
+  end
+
+  test "will create 447123456789" do
+    mobile= Mobile.create! number: "447123456789"
+    assert_not_nil(mobile)
+  end
+  
+  test "will create +447123456789" do
+    mobile= Mobile.create! number: "+447123456789"
+    assert_not_nil(mobile)
+  end
+
+
+
 end
