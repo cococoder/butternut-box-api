77ec48fc090d3b268b115d598ce54e3e4324eb92
dsofadog <delaney@so.fa.dog>
2021-02-16 04:07:12 +0200

    add validations

--- app/models/mobile.rb
+++ app/models/mobile.rb
@@ -1,2 +1,10 @@
 class Mobile < ApplicationRecord
+  validates :number,uniqueness: true, presence: true
+  validate :validate_mobile_number
+  def validate_mobile_number
+    unless /^(44|\+44|0)7\d{9}$/.match(number)
+      errors.add(:number,"#{number} is not a valid uk mobile number")
+    end
+  end
+
 end
